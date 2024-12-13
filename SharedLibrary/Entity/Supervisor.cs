7d1615187cf24a78c92176d11f7858c08908328e
Martin <contomartin11@gmail.com>
2024-12-13 10:35:22 +0100

    Added entites and types support

---
+++ SharedLibrary/Entity/Supervisor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Entity
+{
+    public class Supervisor : BaseEntity
+    {
+        [MaxLength(100)]
+        public string FirstName {  get; set; } = string.Empty;
+        [MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+    }
+}
---
+++ SharedLibrary/Entity/Theme.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedLibrary.Types;
+
+namespace SharedLibrary.Entity
+{
+    public class Theme : BaseEntity
+    {
+        [MaxLength(255)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public required Supervisor Supervisor {  get; set; }
+
+        [Required]
+        public required StProgram StProgram { get; set; }
+
+        public bool IsFullTimeStudy {  get; set; }
+
+        public bool IsExternalStudy { get; set; }
+
+        public ResearchType ResearchType { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
